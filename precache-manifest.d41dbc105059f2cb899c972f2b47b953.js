be869c03143987a9467be93ab2e9144727a43cc0
vikas6577 <vs509411@gmail.com>
2022-04-02 13:36:15 +0530

    Updates

---
+++ precache-manifest.d41dbc105059f2cb899c972f2b47b953.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8db27a0c154456cbd826",
+    "url": "/firstreactwebsite/static/css/main.edb4e482.chunk.css"
+  },
+  {
+    "revision": "8db27a0c154456cbd826",
+    "url": "/firstreactwebsite/static/js/main.8db27a0c.chunk.js"
+  },
+  {
+    "revision": "db20f86d0530543db744",
+    "url": "/firstreactwebsite/static/js/1.db20f86d.chunk.js"
+  },
+  {
+    "revision": "3a2e0e0b763789a1abbb",
+    "url": "/firstreactwebsite/static/js/2.3a2e0e0b.chunk.js"
+  },
+  {
+    "revision": "29a6472f5090388d62be",
+    "url": "/firstreactwebsite/static/js/runtime~main.29a6472f.js"
+  },
+  {
+    "revision": "7eb448e99e8fc93ed422a86e6fccd89f",
+    "url": "/firstreactwebsite/index.html"
+  }
+];
