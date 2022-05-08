7abb73a354e44d4705ac4f4f3f2bdbbe5f03351d
vikas6577 <vs509411@gmail.com>
2022-05-08 15:47:46 +0530

    added a comment

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -28,9 +28,12 @@ export default function TextForm(props) {
     // let copyText=document.getElementsByClassName('form-control');
     // copyText.select();
     // navigator.clipboard.writeText(copyText.value);
-    const area = document.querySelector('#myBox')
-    area.select();
-    document.execCommand('copy')
+    // var text=document.getElementById("myBox");
+    // text.select();
+    // navigator.clipboard.writeText(text.value);
+    // document.getSelection().removeAllRanges();   // this lines does not allow the text to be selected in the text area even when it is copied
+
+    navigator.clipboard.writeText(text);
     props.showAlert("Copied to Clipboard","success");
   }
   const handleExtraspaces=()=>{
@@ -42,31 +45,32 @@ export default function TextForm(props) {
   const [text,setText]=useState('');
   // const [text,setText]=useState('Enter text here');
   // text="new text"; // wrong way to change the state
-  // setText("new text");
+  // setText("new text");  // this is the right way to do so
   return (
     <>
     <div className="container" style={{color:props.mode==='dark'?'white':'#042743'}}>
-        <h1>{props.heading}</h1>
+        <h1 className='mb-2'>{props.heading}</h1>
         <div className="mb-3">
           
-            <textarea className="form-control" value={text} onChange={handleOnChange} style={ {backgroundColor:props.mode==='dark'?'grey':'white',color:props.mode==='dark'?'white':'#042743'}} id="myBox" rows="8" placeholder='Enter text here' ></textarea>
+            <textarea className="form-control" value={text} onChange={handleOnChange} style={ {backgroundColor:props.mode==='dark'?'#13466e':'white',color:props.mode==='dark'?'white':'#042743'}} id="myBox" rows="8" placeholder='Enter text here' ></textarea>
         </div>
-        <button className='btn btn-primary mx-2' onClick={handleUpClick}>Convert to UpperCase</button>
-        <button className='btn btn-warning mx-2' onClick={handleDownClick} >Convert to Lowercase</button>
-        <button className='btn btn-danger mx-2' onClick={handleClear} >Clear</button>
-        <button className='btn btn-success mx-2' onClick={handleCopy} >Copy Text</button>
-        <button className='btn btn-primary mx-2' onClick={handleExtraspaces} >Remove Spaces</button>
+        <button disabled={text.length===0} className='btn btn-primary mx-2 my-2' onClick={handleUpClick}>Convert to UpperCase</button>
+        <button disabled={text.length===0} className='btn btn-primary mx-2 my-2' onClick={handleDownClick} >Convert to Lowercase</button>
+        <button disabled={text.length===0} className='btn btn-primary mx-2 my-2' onClick={handleClear} >Clear</button>
+        <button disabled={text.length===0} className='btn btn-primary mx-2 my-2' onClick={handleCopy} >Copy Text</button>
+        <button disabled={text.length===0} className='btn btn-primary mx-2 my-2' onClick={handleExtraspaces} >Remove Spaces</button>
 
     </div>
     <div className="container my-3 " style={{color:props.mode==='dark'?'white':'#042743'}}>
       <h1>Your text summary</h1>
-      <p>{text.split(" ").length} words and {text.length} characters</p>
-      <p>{0.008*text.split(" ").length} Minutes reading time</p>
+      {/* <p>{text.split(" ").filter((element)=>{return element.length!==0}).length} words and {text.length} characters</p> */}
+      <p>{text.split(/\s+/).filter((element)=>{return element.length!==0}).length} words and {text.length} characters</p>          {/*Now if you will write any text in next line earlier it was not included but now it will be included  because now we are using /\s+/  it is used to split on the basis of whitespaces and new line is also considered as */}
+      <p>{0.008*text.split(" ").filter((element)=>{return element.length!==0}).length} Minutes reading time</p>
       <h2>Preview</h2>
-      <p>{text.length>0?text:"Enter something in the textbox above to preview it here"}</p>
+      <p>{text.length>0?text:"Nothing to preview"}</p>
        
     </div>
     </>
   )
 }
- 
+//  hello
